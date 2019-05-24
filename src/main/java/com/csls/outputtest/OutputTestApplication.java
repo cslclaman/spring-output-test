e7797e5bb7a2ed4c7bbe5588501474acf4a9e7a4
Caique Siqueira <cslclaman@gmail.com>
2019-05-24 07:47:15 -0300

    Initialize Spring project

---
+++ src/main/java/com/csls/outputtest/OutputTestApplication.java
@@ -0,0 +1,13 @@
+package com.csls.outputtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutputTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutputTestApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/csls/outputtest/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.csls.outputtest;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OutputTestApplication.class);
+	}
+
+}
